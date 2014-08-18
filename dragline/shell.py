8c6999c3961e3f3b07907c8a0a59b3028db3157e
Ashwin Rajeev <ashwin@quadloops.com>
2014-08-18 20:24:17 +0530

    dragline shell

---
+++ dragline/shell.py
@@ -0,0 +1,86 @@
+from dragline.http import Request
+from dragline.htmlparser import HtmlParser
+import argparse
+import webbrowser
+
+data = {}
+
+
+def start_python_console(namespace=None, noipython=False, banner=''):
+    """Start Python console binded to the given namespace. If IPython is
+    available, an IPython console will be started instead, unless `noipython`
+    is True. Also, tab completion will be used on Unix systems.
+    """
+    if namespace is None:
+        namespace = {}
+
+    try:
+        try:  # use IPython if available
+            if noipython:
+                raise ImportError()
+            try:
+                try:
+                    from IPython.terminal import embed
+                except ImportError:
+                    from IPython.frontend.terminal import embed
+                sh = embed.InteractiveShellEmbed(banner1=banner)
+            except ImportError:
+                from IPython.Shell import IPShellEmbed
+                sh = IPShellEmbed(banner=banner)
+            sh(global_ns={}, local_ns=namespace)
+        except ImportError:
+            import code
+            try:  # readline module is only available on unix systems
+                import readline
+            except ImportError:
+                pass
+            else:
+                import rlcompleter
+                readline.parse_and_bind("tab:complete")
+            code.interact(banner=banner, local=namespace)
+    except SystemExit:  # raised when using exit() in python code.interact
+        pass
+
+
+def shelp():
+    intro = """\n[d] Available Dragline objects:
+    [d]   parser              %(parser)s
+    [d]   request           <%(request)s>
+    [d]   response          %(response)s
+    [d] Useful shortcuts: ## Override methods in Cmd object ##
+    [d]   shelp()           Shell help (print this help)
+    [d]   fetch(url)        Fetch request (or URL) and update local objects
+    [d]   view(response)    View response in a browser\n\n""" % data
+    print(intro)
+
+
+def fetch(murl):
+    global data
+    data["request"] = Request(murl)
+    data["response"] = data["request"].send()
+    data["parser"] = HtmlParser(data["response"])
+    data["url"] = murl
+
+
+def view():
+    global data
+    print(webbrowser.open(data["url"]))
+
+data["fetch"] = fetch
+data["view"] = view
+data["shelp"] = shelp
+
+
+def execute():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'url', action='store', default='', help='url')
+    url = (parser.parse_args()).url
+    fetch(url)
+    shelp()
+    start_python_console(data)
+
+
+if __name__ == "__main__":
+    execute()
+
