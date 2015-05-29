e61de6b38026c8946a8cf989672199de6de004bd
Ashwin Rajeev <ashwin@inzyte.com>
2015-05-29 12:37:52 +0530

    disable selenium test

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,25 +1,25 @@
-import unittest
-from dragline.http import Request
-from dragline.http.selenium import Firefox
-from dragline import runtime
-from requests.compat import urljoin
+#import unittest
+#from dragline.http import Request
+#from dragline.http.selenium import Firefox
+#from dragline import runtime
+#from requests.compat import urljoin
 
 
-HTTPBIN = 'http://httpbin.org/'
-request_processor = Firefox()
+#HTTPBIN = 'http://httpbin.org/'
+#request_processor = Firefox()
 
 
-def httpbin(*suffix):
-    """Returns url for HTTPBIN resource."""
-    return urljoin(HTTPBIN, '/'.join(suffix))
+#def httpbin(*suffix):
+    #"""Returns url for HTTPBIN resource."""
+    #return urljoin(HTTPBIN, '/'.join(suffix))
 
 
-class RequestTest(unittest.TestCase):
-    def setUp(self):
-        runtime.request_processor = request_processor
-        self.addCleanup(runtime.request_processor.clear)
+#class RequestTest(unittest.TestCase):
+    #def setUp(self):
+        #runtime.request_processor = request_processor
+        #self.addCleanup(runtime.request_processor.clear)
 
-    def test_get(self):
-        runtime.settings.SELENIUM_ARGS['proxy'] = "localhost:3128"
-        response = Request(httpbin('get')).send()
-        #self.assertEqual(httpbin('get'), response.json()['url'])
+    #def test_get(self):
+        #runtime.settings.SELENIUM_ARGS['proxy'] = "localhost:3128"
+        #response = Request(httpbin('get')).send()
+        ##self.assertEqual(httpbin('get'), response.json()['url'])
